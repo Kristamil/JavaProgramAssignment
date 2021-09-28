459104190ee1fca6ac3e7a3653075ecb04feec11
unknown <tamilselvibca97@gmail.com>
2021-09-28 14:19:37 +0530

    first commit

---
+++ src/welcome/FirstProgaram.java
@@ -0,0 +1,11 @@
+package welcome;
+
+public class FirstProgaram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome");
+System.out.println("World");
+	}
+
+}
